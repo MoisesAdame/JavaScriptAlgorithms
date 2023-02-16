089127cd1fa20d487ef4812b4de97e56b8e5e7d1
MoisesAdame <moisesadame01@gmail.com>
2023-02-16 10:53:10 -0600

    UPADTE adelsonVelskyLandisTree.js

--- dataStructures/trees/adelsonVelskyLandisTree.js
+++ dataStructures/trees/adelsonVelskyLandisTree.js
@@ -23,4 +23,133 @@ class AVLTree{
         // -- Atributes
         this.root = null
     }
+
+    // Method that inserts a new node where it should be
+    insert(data){
+        // Node that is going to be inserted.
+        var newNode = new Node(data, null, null)
+        if(this.root === null){
+            this.root = newNode
+        }else{
+            this.#insert(this.root, newNode)
+        }
+    }
+
+    #insert(currNode, newNode){
+        if(currNode.data <= newNode.data){
+            if(currNode.right === null){
+                currNode.right = newNode
+            }else{
+                this.#insert(currNode.right, newNode)
+            }
+        }else if(currNode.data > newNode.data){
+            if(currNode.left === null){
+                currNode.left = newNode
+            }else{
+                this.#insert(currNode.left, newNode)
+            }
+        }
+    }
+
+    // Method that calculates the height of a given subtree.
+    height(){
+        return this.#height(this.root)
+    }
+
+    #height(root){
+        if(root === null){
+            return 0
+        }else if(root.left === null && root.right === null){
+            return 0
+        }else if(root.left === null){
+            return 1 + this.height(root.right)
+        }else if(root.right === null){
+            return 1 + this.height(root.left)
+        }else{
+            var leftHeight = this.height(root.left)
+            var rightHeight = this.height(root.right)
+
+            return 1 + leftHeight >= rightHeight ? leftHeight : rightHeight
+        }
+    }
+
+    // Method that calculates the height of a given node.
+    #balanceFactor(node){
+        return this.#height(node.right) - this.#height(node.left)
+    }
+
+    // Method that prints the preOrder traversal walk of the Tree.
+    preOrder(){
+        if(this.root === null){
+            throw 'Empty tree.'
+        }else{
+            this.#preOrder(this.root)
+        }
+    }
+
+    #preOrder(root){
+        if(root.left !== null && root.right !== null){
+            root.print()
+            this.#preOrder(root.left)
+            this.#preOrder(root.right)
+        }else if(root.left !== null){
+            root.print()
+            this.#preOrder(root.left)
+        }else if(root.right !== null){
+            root.print()
+            this.#preOrder(root.right)
+        }else{
+            root.print()
+        }
+    }
+
+    // Method that prints the inOrder traversal walk of the Tree.
+    inOrder(){
+        if(this.root === null){
+            throw 'Empty tree.'
+        }else{
+            this.#inOrder(this.root)
+        }
+    }
+
+    #inOrder(root){
+        if(root.left !== null && root.right !== null){
+            this.#inOrder(root.left)
+            root.print()
+            this.#inOrder(root.right)
+        }else if(root.left !== null){
+            this.#inOrder(root.left)
+            root.print()
+        }else if(root.right !== null){
+            root.print()
+            this.#inOrder(root.right)
+        }else{
+            root.print()
+        }
+    }
+
+    // Method that prints the postOrder traversal walk of the Tree.
+    postOrder(){
+        if(this.root === null){
+            throw 'Empty tree.'
+        }else{
+            this.#postOrder(this.root)
+        }
+    }
+
+    #postOrder(root){
+        if(root.left !== null && root.right !== null){
+            this.#postOrder(root.left)
+            this.#postOrder(root.right)
+            root.print()
+        }else if(root.left !== null){
+            this.#postOrder(root.left)
+            root.print()
+        }else if(root.right !== null){
+            this.#postOrder(root.right)
+            root.print()
+        }else{
+            root.print()
+        }
+    }
 }
