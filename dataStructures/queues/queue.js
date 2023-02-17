1daca6808f6ae7041f55ba2e9b123362af309b14
MoisesAdame <moisesadame01@gmail.com>
2023-02-16 22:19:38 -0600

    CREATE queue.js

---
+++ dataStructures/queues/queue.js
@@ -0,0 +1,13 @@
+// Author: Moisés Adame-Aguilar
+// Date: February 16, 2023
+// Descrption: Simple Queue.
+
+import { doublyLinkedList } from "../linkedLists/doublyLinkedList.js";
+
+class queue extends doublyLinkedList{
+    // -- Methods
+    // Constructor method.
+    constructor(){
+        super()
+    }
+}
