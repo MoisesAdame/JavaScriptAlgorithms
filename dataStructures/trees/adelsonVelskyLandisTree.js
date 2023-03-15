8c5e12dcc5320fefd542d65ab18378c388e842b0
MoisesAdame <moisesadame01@gmail.com>
2023-03-15 12:12:08 -0600

    AVLTree done, maybe add optimization in double rotations.

--- dataStructures/trees/adelsonVelskyLandisTree.js
+++ dataStructures/trees/adelsonVelskyLandisTree.js
@@ -1,5 +1,5 @@
 // Author: Moisés Adame-Aguilar
-// Date: February 15, 2023
+// Date: March 15, 2023
 // Descrption: AVL Tree.
 
 // Node class for every element in the LinkedList
@@ -10,6 +10,10 @@ class Node{
         this.data = data;
         this.left = null;
         this.right = null;
+    }
+
+    print(){
+        console.log(this.data)
     }
 }
 
@@ -77,6 +81,24 @@ class AVLTree{
         return midTree
     }
 
+    // Method that makes a double right rotation.
+    doubleRightRotation(root){
+        var topTree = root
+        var midTree = topTree.left
+        var bottomTree = midTree.right
+        var bottomTreeLeft = bottomTree.left
+        var bottomTreeRight = bottomTree.right
+
+        midTree.right = bottomTreeLeft
+        topTree.left = bottomTreeRight
+
+        bottomTree.left = midTree
+        bottomTree.right = topTree
+
+        return bottomTree
+    }
+
+    // Method that makes a simple left rotation.
     leftRotation(root){
         var topTree = root
         var midTree = topTree.right
@@ -88,16 +110,113 @@ class AVLTree{
         return midTree
     }
 
+    // Method that makes a double left rotation.
+    doubleLeftRotation(root){
+        var topTree = root
+        var midTree = topTree.right
+        var bottomTree = midTree.left
+        var bottomTreeLeft = bottomTree.left
+        var bottomTreeRight = bottomTree.right
+
+        midTree.left = bottomTreeLeft
+        topTree.right = bottomTreeRight
+
+        bottomTree.left = topTree
+        bottomTree.right = midTree
+
+        return bottomTree
+    }
+
+    // Method that given a subtree root, balances the subtree using balanceFactor()
     balance(root){
         if(this.balanceFactor(root) < -1){
             if(this.balanceFactor(root.left) < 0){
                 return this.rightRotation(root)
+            }else if(this.balanceFactor(root.left) > 0){
+                return this.doubleRightRotation(root)
             }
         }else if(this.balanceFactor(root) > 1){
             if(this.balanceFactor(root.right) > 0){
                 return this.leftRotation(root)
+            }else if(this.balanceFactor(root.right) < 0){
+                return this.doubleLeftRotation(root)
             }
         }
         return root
+    }
+
+    // Method that prints the preOrder traversal walk of the Tree.
+    preOrder(){
+        if(this.root === null){
+            throw 'Empty tree.'
+        }else{
+            this.#preOrder(this.root)
+        }
+    }
+
+    #preOrder(root){
+        if(root.left !== null && root.right !== null){
+            root.print()
+            this.#preOrder(root.left)
+            this.#preOrder(root.right)
+        }else if(root.left !== null){
+            root.print()
+            this.#preOrder(root.left)
+        }else if(root.right !== null){
+            root.print()
+            this.#preOrder(root.right)
+        }else{
+            root.print()
+        }
+    }
+
+    // Method that prints the inOrder traversal walk of the Tree.
+    inOrder(){
+        if(this.root === null){
+            throw 'Empty tree.'
+        }else{
+            this.#inOrder(this.root)
+        }
+    }
+
+    #inOrder(root){
+        if(root.left !== null && root.right !== null){
+            this.#inOrder(root.left)
+            root.print()
+            this.#inOrder(root.right)
+        }else if(root.left !== null){
+            this.#inOrder(root.left)
+            root.print()
+        }else if(root.right !== null){
+            root.print()
+            this.#inOrder(root.right)
+        }else{
+            root.print()
+        }
+    }
+
+    // Method that prints the postOrder traversal walk of the Tree.
+    postOrder(){
+        if(this.root === null){
+            throw 'Empty tree.'
+        }else{
+            this.#postOrder(this.root)
+        }
+    }
+
+    #postOrder(root){
+        if(root.left !== null && root.right !== null){
+            this.#postOrder(root.left)
+            this.#postOrder(root.right)
+            root.print()
+        }else if(root.left !== null){
+            this.#postOrder(root.left)
+            root.print()
+        }else if(root.right !== null){
+            this.#postOrder(root.right)
+            root.print()
+        }else{
+            root.print()
+        }
     }
 }
