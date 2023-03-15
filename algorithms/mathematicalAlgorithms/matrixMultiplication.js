6d5176a6f35c97c374ac811991826fab3d0f02a0
MoisesAdame <moisesadame01@gmail.com>
2023-03-15 09:44:02 -0600

    advances in AVLTree

--- algorithms/mathematicalAlgorithms/matrixMultiplication.js
+++ algorithms/mathematicalAlgorithms/matrixMultiplication.js
@@ -3,9 +3,38 @@
 // Descrption: Multiplication of two matrices.
 
 function matrixMultiplication(matrix1, matrix2){
-    if(true){
-        ;
+    // Validation of the operation
+    var validOperation = matrix1[0].length === matrix2.length
+    if(validOperation){
+        // Rows and columns of the result matrix
+        var resRows = matrix1.length
+        var resCols = matrix2[0].length
+
+        // Zero matrix will be filled.
+        var resMatrix = []
+        var row = []
+        for(var i = 0; i < resRows; i++){
+            row = []
+            for(var j = 0; j < resCols; j++){
+                row.push(0)
+            }
+            resMatrix.push(row)
+        }
+        //console.log(resMatrix)
+
+        for(var i = 0; i < resRows; i++){
+            for(var j = 0; j < resCols; j++){
+                resMatrix[j][i] += matrix1[i][j] * matrix2[j][i]
+            }
+        }
+
+        return resMatrix
     }else{
-        throw 'Matrices must be the same dimension.'
+        throw 'Left matrix should have the same number of columns as rows in right matrix.'
     }
 }
+
+var matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+var matrix2 = [[2, 1], [3, 2], [4, 5]]
+
+console.log(matrixMultiplication(matrix1, matrix2))
--- dataStructures/trees/adelsonVelskyLandisTree.js
+++ dataStructures/trees/adelsonVelskyLandisTree.js
@@ -1,19 +1,15 @@
 // Author: Moisés Adame-Aguilar
-// Date: February 16, 2023
-// Descrption: Adelson-Velsky and Landis Tree
+// Date: February 15, 2023
+// Descrption: AVL Tree.
 
 // Node class for every element in the LinkedList
 class Node{
     // -- Methods
-    constructor(data, left, right){
+    constructor(data){
         // -- Atributes
-        this.data = data
-        this.left = left
-        this.right = right
-    }
-
-    print(){
-        console.log(this.data)
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
 }
 
@@ -24,159 +20,97 @@ class AVLTree{
         this.root = null
     }
 
-    // Method that inserts a new node where it should be
+    // Method that allows the insertion of elements inside the tree.
     insert(data){
-        // Node that is going to be inserted.
-        var newNode = new Node(data, null, null)
-        if(this.root === null){
-            this.root = newNode
+        this.root = this.#insert(this.root, data)
+    }
+
+    #insert(root, data){
+        if(root === null){
+            return new Node(data)
         }else{
-            this.#insert(this.root, newNode)
-        }
-        this.#checkBalance(this.root)
-    }
-
-    #insert(currNode, newNode){
-        if(currNode.data <= newNode.data){
-            if(currNode.right === null){
-                currNode.right = newNode
+            if(root.data >= data){
+                root.left = this.#insert(root.left, data)
             }else{
-                this.#insert(currNode.right, newNode)
-            }
-        }else if(currNode.data > newNode.data){
-            if(currNode.left === null){
-                currNode.left = newNode
-            }else{
-                this.#insert(currNode.left, newNode)
+                root.right = this.#insert(root.right, data)
             }
         }
+
+        return this.balance(root)
     }
 
-    // Method that calculates the height of a given subtree.
-    height(){
-        return this.#height(this.root)
-    }
-
-    #height(root){
+    // Method that calculates the height of a given subtree root.
+    height(root){
         if(root === null){
             return 0
         }else if(root.left === null && root.right === null){
             return 1
-        }else if(root.left === null){
-            return 1 + this.#height(root.right)
         }else if(root.right === null){
-            return 1 + this.#height(root.left)
+            return 1 + this.height(root.left)
+        }else if(root.left === null){
+            return 1 + this.height(root.right)
         }else{
-            var leftHeight = this.#height(root.left)
-            var rightHeight = this.#height(root.right)
-
-            return 1 + (leftHeight >= rightHeight ? leftHeight : rightHeight)
-        }
-    }
-
-    // Method that calculates the height of a given node.
-    #balanceFactor(node){
-        return this.#height(node.right) - this.#height(node.left)
-    }
-
-    // Method that checks if the AVLTree is balanced.
-    #checkBalance(root){
-        var balanceFactor = this.#balanceFactor(root)
-        if(root.left !== null && root.right !== null){
-            this.#checkBalance(root.left)
-            if(balanceFactor > 1 || balanceFactor < -1 ){
-                ;
-            }
-            this.#checkBalance(root.right)
-        }else if(root.left !== null){
-            this.#checkBalance(root.left)
-            if(balanceFactor > 1 || balanceFactor < -1 ){
-                ;
-            }
-        }else if(root.right !== null){
-            if(balanceFactor > 1 || balanceFactor < -1 ){
-                ;
-            }
-            this.#checkBalance(root.right)
-        }else{
-            if(balanceFactor > 1 || balanceFactor < -1 ){
-                ;
+            var leftHeight = this.height(root.left)
+            var rightHeight = this.height(root.right)
+            if(rightHeight >= leftHeight){
+                return 1 + rightHeight
+            }else{
+                return 1 + leftHeight
             }
         }
     }
 
-    // Method that prints the preOrder traversal walk of the Tree.
-    preOrder(){
-        if(this.root === null){
-            throw 'Empty tree.'
-        }else{
-            this.#preOrder(this.root)
-        }
+    // Method that calculates the balance factor of a given subtree root.
+    balanceFactor(root){
+        return this.height(root.right) - this.height(root.left)
     }
 
-    #preOrder(root){
-        if(root.left !== null && root.right !== null){
-            root.print()
-            this.#preOrder(root.left)
-            this.#preOrder(root.right)
-        }else if(root.left !== null){
-            root.print()
-            this.#preOrder(root.left)
-        }else if(root.right !== null){
-            root.print()
-            this.#preOrder(root.right)
-        }else{
-            root.print()
-        }
+    // Method that makes a simple right rotation.
+    rightRotation(root){
+        var topTree = root
+        var midTree = topTree.left
+        var midTreeRight = topTree.left.right
+        topTree.left = midTreeRight
+        midTree.right = topTree
+
+        return midTree
     }
 
-    // Method that prints the inOrder traversal walk of the Tree.
-    inOrder(){
-        if(this.root === null){
-            throw 'Empty tree.'
-        }else{
-            this.#inOrder(this.root)
-        }
+    leftRotation(root){
+        var topTree = root
+        var midTree = topTree.right
+        var midTreeLeft = topTree.right.left
+
+        topTree.right = midTreeLeft
+        midTree.left = topTree
+
+        return midTree
     }
 
-    #inOrder(root){
-        if(root.left !== null && root.right !== null){
-            this.#inOrder(root.left)
-            root.print()
-            this.#inOrder(root.right)
-        }else if(root.left !== null){
-            this.#inOrder(root.left)
-            root.print()
-        }else if(root.right !== null){
-            root.print()
-            this.#inOrder(root.right)
-        }else{
-            root.print()
+    balance(root){
+        if(this.balanceFactor(root) < -1){
+            if(this.balanceFactor(root.left) < 0){
+                return this.rightRotation(root)
+            }
+        }else if(this.balanceFactor(root) > 1){
+            if(this.balanceFactor(root.right) > 0){
+                return this.leftRotation(root)
+            }
         }
+        return root
     }
 
-    // Method that prints the postOrder traversal walk of the Tree.
-    postOrder(){
-        if(this.root === null){
-            throw 'Empty tree.'
-        }else{
-            this.#postOrder(this.root)
-        }
-    }
 
-    #postOrder(root){
-        if(root.left !== null && root.right !== null){
-            this.#postOrder(root.left)
-            this.#postOrder(root.right)
-            root.print()
-        }else if(root.left !== null){
-            this.#postOrder(root.left)
-            root.print()
-        }else if(root.right !== null){
-            this.#postOrder(root.right)
-            root.print()
-        }else{
-            root.print()
-        }
-    }
 }
+
+
+let myTree = new AVLTree()
+myTree.insert(12)
+myTree.insert(15)
+myTree.insert(20)
+
+// console.log(myTree)
+// console.log(myTree.height(myTree.root.left))
+// console.log(myTree.height(myTree.root.right))
+// myTree.root = myTree.balance(myTree.root)
+console.log(myTree)
